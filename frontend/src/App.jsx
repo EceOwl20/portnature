5cf4320919991a1e4f5783af85b7eeb994ef4b27
Ece Bayramer <ecebayramer20@gmail.com>
2024-11-20 15:36:48 +0300

    icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './components/header/Header'
+import Homepage from './pages/Homepage'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Homepage/>
+      
     </div>
   )
 }
---
+++ frontend/src/components/homepage/Reservation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Reservation = () => {
+  return (
+    <div className='flex w-screen h-auto items-center justify-center bg-[#f8f8f8] '>
+      <div className='flex w-[60%] text-[#233038] font-monserrat font-light text-[20px] leading-normal text-center py-[100px]'>
+        <button className=' border-[0.7px] w-[25%] border-[#00000033] py-[15px] px-[50px] bg-white max-h-[60px]'>Check-in</button>
+        <button className='border-[0.7px] w-[25%] border-[#00000033]  py-[15px] px-[50px] bg-white max-h-[60px]'>Check-Out</button>
+        <button className='border-[0.7px] w-[25%] border-[#00000033]  py-[15px] px-[50px] bg-white max-h-[60px]'>Guest</button>
+        <button className='border-[0.7px] w-[25%] border-[#00000033]  py-[15px] px-[50px] font-bold text-white bg-[#233038] max-w-[304px] max-h-[60px]'>Book Now</button>
+      </div>
+    </div>
+  )
+}
+
+export default Reservation
