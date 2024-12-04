796a10e284d600b9387326d3409a231a15f243b6
Ece Bayramer <ecebayramer20@gmail.com>
2024-12-04 15:45:06 +0300

    carousel

--- frontend/src/components/BorderCarousel.jsx
+++ frontend/src/components/BorderCarousel.jsx
@@ -10,7 +10,7 @@ const header=["","","",""]
 const BorderCarousel = () => {
     const [emblaRef, emblaApi] = useCarousel({
         loop: true,
-        align:"center",
+        align:"start",
         startIndex:0,
       }, [
         Autoplay({ delay: 2000 }),
@@ -51,22 +51,32 @@ const BorderCarousel = () => {
       }, [emblaApi, onSelect]);
       
   return (
-    <div className="flex flex-col lg:w-11/12 xl:w-full w-full h-auto items-end justify-end my-40">
-    <div className="overflow-hidden relative h-auto w-5/6" ref={emblaRef}>
+    <div className="flex flex-col lg:w-11/12 xl:w-full h-auto items-end justify-end my-40">
+    <div className="overflow-hidden relative xl:h-[698px] w-[90%] lg:w-[75%]" ref={emblaRef}>
         <div className="flex grid-flow-col">
           {images.map((image, index) => (
-            <div className="flex-[0_0_auto] mx-1 md:mx-2 sm:w-[calc(50%-1rem)] md:w-[calc(33.3%-1rem)] lg:mx-4 xl:mx-2 lg:w-[calc(33.3%-1rem)] xl:w-[calc(33.3%-0.5rem)] w-11/12 flex justify-center relative group" key={index}>
+            <div className="flex-[0_0_auto] mx-1 sm:w-[calc(50%-1rem)] lg:mx-4 xl:mx-2 lg:w-[calc(33.3%-1rem)] xl:w-[calc(33.3%-0.5rem)]  w-11/12 flex justify-center relative group" key={index}>
+              <div className="flex flex-col relative z-10 xl:max-w-[553px]">
               <img
-                className="cursor-pointer overflow-hidden object-cover"
+                className="cursor-pointer overflow-hidden object-cover relative"
                 height={589}
                 width={348}
                 layout="responsive"
                 src={image}
                 alt={`Slide ${index + 1}`}
               />
+              <div className="flex w-full items-end justify-end">
+                <div className="flex text-start justify-start items-center pt-[3%] w-[80%]">
+                    <p className="text-[12px] text-black font-monserrat font-normal leading-[18px] w-[88%]">A luxurious holiday  ones is waiting for you in Family Rooms, designed in the comfort of your home</p>
+                </div>
+              </div>
+              </div>
 
               <div className="absolute flex flex-col top-4 text-start items-center justify-center ">
                 <span className="text-[18px] font-lora lg:text-[25px] leading-normal text-white uppercase font-medium ">{header[index]}</span>
+              </div>
+              <div className="absolute top-[8%] left-[18%] border border-[#CFCFCF] h-[55vh] w-[70%] z-1">
+
               </div>
               
             
