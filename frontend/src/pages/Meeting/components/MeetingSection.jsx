d08330a822d5d4315b3b5ffe508e75d93b257811
Ece Bayramer <ecebayramer20@gmail.com>
2025-01-29 17:55:39 +0300

    spa edited

--- frontend/src/pages/Meeting/components/MeetingSection.jsx
+++ frontend/src/pages/Meeting/components/MeetingSection.jsx
@@ -3,14 +3,14 @@ import Underline from "../../../svg/UnderLine/UnderLine"
 
 const MeetingSection = ({image,header,text,span}) => {
   return (
-    <div id='thermesos' className='flex w-full justify-center items-center my-10'>
+    <div id='thermesos' className='flex w-full justify-center items-center my-12'>
       <div className='flex flex-col lg:flex-row w-[90%] justify-between items-center'>
-        <img src={image.firebaseUrl} alt={image.altText} className='w-[60%]'/>
-        <div className='flex flex-col w-[35%] items-start justify-start text-start'>
-           <div className='w-[70%] flex flex-col gap-[30px]'>
-           <h2 className='text-[40px] text-customGray font-lora leading-normal font-medium'>{header}</h2>
+        <img src={image.firebaseUrl} alt={image.altText} className='w-[95%] lg:w-[60%]'/>
+        <div className='flex flex-col lg:w-[35%] items-center lg:items-start justify-center lg:justify-start text-center lg:text-start '>
+           <div className='w-[85%] lg:w-[70%] flex flex-col gap-5 lg:gap-[30px] mt-4'>
+           <h2 className='text-[25px] lg:text-[40px] text-customGray font-lora leading-normal font-medium'>{header}</h2>
             <Underline/>
-            <p className='text-black text-[15px] font-monserrat font-normal leading-[22.5px]'>{text}</p>
+            <p className='text-black text-[13px] lg:text-[15px] font-monserrat font-normal leading-[22.5px]'>{text}</p>
             <span className='text-[#3D515E] font-monserrat text-[15px] font-bold leading-[22.5px]'>{span}</span>
            </div>
         </div>
